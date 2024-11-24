0cc32543783ac924db7c913d63299169cf8b42e0
simonxiangd <2637466979@qq.com>
2024-11-24 17:44:57 -0500

    update backend url

--- src/components/GenerateModeCard.jsx
+++ src/components/GenerateModeCard.jsx
@@ -33,7 +33,7 @@ const ModePanel = () => {
     // return;
 
     try {
-        const response = await fetch('http://127.0.0.1:3113/process-inputs', {
+        const response = await fetch('http://10.223.160.162:3113/process-inputs', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
