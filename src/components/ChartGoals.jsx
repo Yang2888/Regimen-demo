4f3b79c8dda19c7769b14523d06ec179d99753af
simonxiangd <2637466979@qq.com>
2024-12-05 00:28:18 -0500

    Revert "change some styles"
    This reverts commit d6e1ce4cc62ffa2671df3478fde2c558c1c50ed3.

--- src/components/ChartGoals.jsx
+++ src/components/ChartGoals.jsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { DataContext } from "./dataProcess/dataContext";
 import DateLine from "./Dateline";
 import { zoom } from "d3";
+import { css } from "@emotion/react";
 
 // Custom node rendering function with adjusted toggle button size and margin
 const renderRectSvgNode = ({
@@ -26,7 +27,7 @@ const renderRectSvgNode = ({
           style={{
             display: "flex",
             alignItems: "center",
-            width: "220px",
+            width: "50",
             height: "auto",
             padding: "5px",
             backgroundColor: "#f0f2f5",
@@ -35,13 +36,20 @@ const renderRectSvgNode = ({
           }}
         >
           <div style={{ flex: 1, textAlign: "center" }}>
-            <h3 style={{ fontSize: "15px", margin: 0 }}>
+            <h3 style={{ fontSize: "9px", margin: 0 }}>
               {nodeDatum?.Title && nodeDatum.Title}
             </h3>
             <hr style={{ margin: "4px 0", borderColor: "#d9d9d9" }} />
             {nodeDatum?.Summary && (
-              <div style={{ fontSize: "8px", marginTop: "8px" }}>
-                {nodeDatum.Summary}
+              <div style={{ fontSize: "6px", marginTop: "8px" }}>
+                Summary: {nodeDatum.Summary}
+                <br />
+                Notes by doctor: {nodeDatum.Note}
+                <br />
+
+                Notes by Patient: {nodeDatum.Current_status}
+
+  
               </div>
             )}
           </div>
@@ -49,10 +57,10 @@ const renderRectSvgNode = ({
           <div
             onClick={showDetails}
             style={{
-              width: "30px",
-              height: "30px",
-              marginLeft: "10px",
-              marginRight: "15px",
+              width: "15px",
+              height: "15px",
+              marginLeft: "5px",
+              marginRight: "7.5px",
               backgroundColor: "lightgreen",
               borderRadius: "50%",
               display: "flex",
@@ -62,7 +70,7 @@ const renderRectSvgNode = ({
             }}
           >
             <span
-              style={{ fontSize: "16px", position: "relative", top: "-2.5px" }}
+              style={{ fontSize: "8px", position: "relative", top: "-1.25px" }}
             >
               i
             </span>
@@ -84,7 +92,7 @@ const renderRectSvgNode = ({
                 cursor: "pointer",
               }}
             >
-              <span
+              {/* <span
                 style={{
                   fontSize: "16px",
                   position: "relative",
@@ -92,7 +100,7 @@ const renderRectSvgNode = ({
                 }}
               >
                 {nodeDatum.__rd3t.collapsed ? "+" : "-"}
-              </span>
+              </span> */}
             </div>
           )}
         </div>
